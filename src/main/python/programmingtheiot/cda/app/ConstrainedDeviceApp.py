0c8d2fa29a74471c31ea1d778a027f7c0cb1fb10
Nicolas Gallo <nicolas.gallo800@gmail.com>
2025-02-13 10:26:35 +0100

    PIOT-CDA-02-001 and PIOT-CDA-02-002 implemented

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -11,6 +11,7 @@
 # 
 
 import logging
+from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
 
 from time import sleep
 
@@ -30,7 +31,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("Initializing CDA...")
 		
-		# TODO: implementation here
+		self.sysPerfMgr = SystemPerformanceManager()
 
 	def startApp(self):
 		"""
@@ -39,7 +40,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("Starting CDA...")
 		
-		# TODO: implementation here
+		self.sysPerfMgr.startManager()
 		
 		logging.info("CDA started.")
 
@@ -50,7 +51,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("CDA stopping...")
 		
-		# TODO: implementation here
+		self.sysPerfMgr.stopManager()
 		
 		logging.info("CDA stopped with exit code %s.", str(code))
 		
@@ -73,7 +74,7 @@ def main():
 	cda.startApp()
 	
 	# run for 10 seconds - this can be changed as needed
-	sleep(10)
+	sleep(65)
 	
 	# optionally stop the app - this can be removed if needed
 	cda.stopApp(0)
