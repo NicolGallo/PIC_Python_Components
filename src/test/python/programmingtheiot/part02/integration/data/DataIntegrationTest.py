2d9231865b89936e7b08d5c725f6970adcb4d4a1
Nicolas Gallo <nicolas.gallo800@gmail.com>
2025-02-28 15:58:00 +0100

    DataIntegrationTest.py discommented 3 tests.

--- src/test/python/programmingtheiot/part02/integration/data/DataIntegrationTest.py
+++ src/test/python/programmingtheiot/part02/integration/data/DataIntegrationTest.py
@@ -114,7 +114,7 @@ class DataIntegrationTest(unittest.TestCase):
 		logging.info("ActuatorData JSON from GDA: " + dataStr)
 		logging.info("ActuatorData object: " + str(dataObj))
 
-	@unittest.skip("Ignore for now.")
+	#@unittest.skip("Ignore for now.")
 	def testReadSensorDataFromGdaDataPath(self):
 		logging.info("\n\n----- [SensorData JSON from file to object] -----")
 		
@@ -127,7 +127,7 @@ class DataIntegrationTest(unittest.TestCase):
 		logging.info("SensorData JSON from GDA: " + dataStr)
 		logging.info("SensorData object: " + str(dataObj))
 
-	@unittest.skip("Ignore for now.")
+	#@unittest.skip("Ignore for now.")
 	def testReadSystemPerformanceDataFromGdaDataPath(self):
 		logging.info("\n\n----- [SystemPerformanceData JSON from file to object] -----")
 		
